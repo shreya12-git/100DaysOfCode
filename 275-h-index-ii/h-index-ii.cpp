6892738c9f2238bb98f1a2ba30dd696f288a4217
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-04-13 14:23:02 +0530

    Time: 12 ms (57.17%) | Memory: 21 MB (26.38%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -4,17 +4,21 @@ public:
         int n=citations.size();
         int l=0,r=n-1;
         int ans=0;
+    
         while(l<=r){
             int m=l+(r-l)/2;
-            if(citations[m]>=n-m){
-                ans=max(ans,n-m);
-                r=m-1;
+            int pre=n-m;
+            if(citations[m]==pre){
+                return citations[m];
             }
-            else{
+            else if(citations[m]<pre){
                 l=m+1;
             }
+            else{
+                r=m-1;
+            }
         }
-        return ans;
+        return n-l;
     }
     
 };
