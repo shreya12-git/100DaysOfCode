9154d456c3fc0d9012dcec38e19f2be6c6d82d75
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-04-16 19:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (63.20%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -2,42 +2,64 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
 
-        queue<int> q;
-        int remainingStudents = students.size(); // Track remaining students
+        // queue<int> q;
+        // int remainingStudents = students.size(); // Track remaining students
 
-        // Push all students into the queue
-        for (int student : students) {
-            q.push(student);
+        // // Push all students into the queue
+        // for (int student : students) {
+        //     q.push(student);
+        // }
+
+        // // Process each sandwich
+        // for (int sandwich : sandwiches) {
+        //     if (q.empty()) {
+        //         break; // No more students to process
+        //     }
+
+        //     bool foundSandwich = false;
+
+        //     // Process students in the queue until we find the matching sandwich or reach the end
+        //     int size = q.size();
+        //     for (int i = 0; i < size; ++i) {
+        //         int student = q.front();
+        //         q.pop();
+
+        //         if (student == sandwich) {
+        //             foundSandwich = true;
+        //             remainingStudents--; // Decrement count of remaining students
+        //             break; // Found the matching sandwich
+        //         } else {
+        //             q.push(student); // Put the student back in the queue
+        //         }
+        //     }
+
+        //     if (!foundSandwich) {
+        //         break; // If sandwich not found, stop processing
+        //     }
+        // }
+
+        // return remainingStudents; 
+       int count0=0;
+       int count1=0;
+        for(auto it:students){
+            if(it==0){
+                count0++;
+            }
+            if(it==1){
+                count1++;
+            }
         }
-
-        // Process each sandwich
-        for (int sandwich : sandwiches) {
-            if (q.empty()) {
-                break; // No more students to process
+        for(int i=0;i<sandwiches.size();i++){
+            if(sandwiches[i]==0){
+                count0--;
             }
-
-            bool foundSandwich = false;
-
-            // Process students in the queue until we find the matching sandwich or reach the end
-            int size = q.size();
-            for (int i = 0; i < size; ++i) {
-                int student = q.front();
-                q.pop();
-
-                if (student == sandwich) {
-                    foundSandwich = true;
-                    remainingStudents--; // Decrement count of remaining students
-                    break; // Found the matching sandwich
-                } else {
-                    q.push(student); // Put the student back in the queue
-                }
+            if(sandwiches[i]==1){
+                count1--;
             }
-
-            if (!foundSandwich) {
-                break; // If sandwich not found, stop processing
+            if(count0==-1||count1==-1){
+                return sandwiches.size()-i;
             }
         }
-
-        return remainingStudents; 
+        return 0;
     }
 };
