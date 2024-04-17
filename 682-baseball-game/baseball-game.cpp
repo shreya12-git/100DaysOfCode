a30757bd519c7c48265fbefb290c2671626b3efd
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-04-17 22:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.00%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        int n=operations.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(operations[i] =="C"){
+                st.pop();
+            }
+            else if(operations[i]=="D"){
+                int temp=st.top();
+                temp=temp*2;
+                st.push(temp);
+            }
+            else if(operations[i]=="+"){
+                int temp1=st.top();
+                st.pop();
+                int temp2=st.top();
+                int temp3=temp1+temp2;
+                st.push(temp1);
+                st.push(temp3);
+            }
+            else{
+                int val=stoi(operations[i]);
+                st.push(val);
+            }
+           
+            }
+             int sum=0;
+            while(!st.empty()){
+                int temp=st.top();
+                sum+=temp;
+                st.pop();
+        }
+        return sum;
+    }
+};
