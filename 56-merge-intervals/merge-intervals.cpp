57b8d779f25eab9081743866b5587bb730b66ab6
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-05-01 20:37:40 +0530

    Time: 19 ms (85.87%) | Memory: 22.8 MB (31.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
+    static bool comp(vector<int>&a, vector<int>& b){
+        return a[0] < b[0];
+    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin() ,intervals.end());
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        for(int i=1 ; i< intervals.size() ; i++){
-            if(ans.back()[1] >= intervals[i][0]){
-                if(ans.back()[1] > intervals[i][1]) continue;
-                ans.back()[1]=intervals[i][1];
-            }else{
-                ans.push_back(intervals[i]);
-            }
+        sort(intervals.begin(), intervals.end(), comp);
+        vector<vector<int>> ret;
+        int size = intervals.size();
+        if(size == 0){
+            return ret;
         }
-        return ans;
-    
+        int curs = intervals[0][0], curf = intervals[0][1];
+        for(int i=1;i<size;i++){
+            if(intervals[i][0] <= curf){
+                curf = max(intervals[i][1], curf);
+                continue;
+            }
+            ret.push_back(vector<int>{curs,curf});
+            curs = intervals[i][0];
+            curf = intervals[i][1];
+        }
+        ret.push_back(vector<int>{curs,curf});
+        return ret;
+        
     }
 };
