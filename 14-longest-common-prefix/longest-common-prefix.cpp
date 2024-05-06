e3b3aa731653dcac1861374a65ee507311cb31fb
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-05-06 19:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (20.86%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        string ans="";
+        sort(strs.begin(),strs.end());
+        for(int i=0;i<n;i++){
+            cout<<strs[i]<<endl;
+        }
+        string first=strs[0],last=strs[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
+            }
+            ans+=first[i];
+        }
+            return ans;      
+    }
+};
