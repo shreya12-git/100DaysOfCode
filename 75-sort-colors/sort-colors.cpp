829d450f815284319b58c1546c0e7d17b62ce7f2
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-05-26 23:30:07 +0530

    Time: 4 ms (30.70%) | Memory: 9.7 MB (94.96%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+      int n=nums.size();
+      int min;
+      for(int i=0;i<n-1;i++){
+          min=nums[i];
+          for(int j=i+1;j<n;j++){
+              if(min>nums[j]){
+                  swap(min,nums[j]);
+              }
+          }
+          nums[i]=min;
+      }
+    }
+};
