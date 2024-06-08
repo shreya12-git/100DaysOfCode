b43df8af47a4cc530b092a9a259c9ae0a55c605e
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-06-08 12:05:52 +0530

    Time: 5 ms (48.72%) | Memory: 7.9 MB (51.33%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        // stack<char>st;
-        // string ans;
-        // for(auto a:s){
-        //     if(a=='('){
-        //         if(st.size()>0){
-        //             ans+='(';
-        //         }
-        //         st.push('(');
-        //     }
-        //     else{
-        //         if(st.size()>1){
-        //             ans+=')';
-        //         }
-        //         st.pop();
-        //     }
-        // }
-        // return ans;
-        string res;
+        string ans="";
         int open=0;
         for(char ch:s){
             if(ch=='('){
                 if(open>0){
-                    res+=ch;
+                    ans+=ch;
                 }
                 open++;
             }
             else{
                 open--;
                 if(open>0){
-                    res+=ch;
+                    ans+=ch;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
