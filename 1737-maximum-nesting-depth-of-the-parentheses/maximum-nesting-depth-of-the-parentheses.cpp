0fcfc49e8f8d1af7e89e22ab0173b206cb56fffe
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-06-09 21:45:37 +0530

    Time: 3 ms (41.61%) | Memory: 7.4 MB (79.84%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        // int count=0;
-        // int max_sum=0;
-        // for(char ch:s){
-        //     if(ch=='('){
-        //         count++;
-        //         if(max_sum<count){
-        //             max_sum=count;
-        //         }
-        //     }
-        //     else if(ch==')'){
-        //             count--;
-        //     }
-        // }
-        // return max_sum;
-        stack<char>st;
-        int mx=0;
-        for(char ch:s){
-            if(ch=='('){
-                st.push(ch);
-            }
-            else if(ch==')'){
-                mx=max((int)st.size(),mx);
-                st.pop();
-            }
+       int maxi=0;
+       int count=0;
+       int n=s.length();
+       for(char c:s){
+        if(c=='('){
+            count++;
         }
-        return mx;
+        if(c==')'){
+            count--;
+        }
+        maxi=max(count,maxi);
+       } 
+       return maxi;
     }
 };
