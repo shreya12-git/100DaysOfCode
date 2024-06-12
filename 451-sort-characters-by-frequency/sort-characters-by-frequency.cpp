66f654ab6e861a2a4362340c1c100bd369ba46a0
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-06-12 16:34:41 +0530

    Time: 13 ms (37.21%) | Memory: 10.8 MB (70.40%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,31 +1,25 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    std::string frequencySort(std::string s) {
-        // Vector to store the frequency and character
-        std::vector<std::pair<int, char>> hash(128, {0, 0});
-        
-        // Fill the vector with character frequencies
-        for (char c : s) {
-            hash[c].first++;
-            hash[c].second = c;
+    string frequencySort(string s) {
+        int n = s.length();
+        unordered_map<char, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[s[i]]++;
         }
-
-        // Sort the vector based on frequencies in descending order
-        std::sort(hash.begin(), hash.end(), [](const std::pair<int, char>& a, const std::pair<int, char>& b) {
-            return a.first > b.first;
-        });
         
-        // Build the result string based on sorted frequencies
-        std::string res;
-        for (const auto& p : hash) {
-            if (p.first > 0) {
-                res.append(p.first, p.second);
-            }
+        // Use a max heap to sort characters by frequency
+        priority_queue<pair<int, char>> maxh;
+        for (auto& i : mp) {
+            maxh.push(make_pair(i.second, i.first));
         }
-        return res;
+        
+        string result = "";
+        while (!maxh.empty()) {
+            pair<int, char> p = maxh.top();
+            maxh.pop();
+            result.append(p.first, p.second);
+        }
+        
+        return result;
     }
 };
