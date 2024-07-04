7c7cdee924663f5b29bbe9f510a44054ac704ebb
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-07-04 12:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (27.40%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,43 +1,26 @@
-#include <vector>
-#include <stack>
-#include <unordered_map>
-
-using namespace std;
-
 class Solution {
 public:
-    vector<int> findGreatest(vector<int>& vec) {
-        vector<int> ans(vec.size());
-        stack<int> st;
-        
-        for (int i = vec.size() - 1; i >= 0; --i) {
-            while (!st.empty() && st.top() <= vec[i]) {
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums2.size();
+        stack<int>st;
+        unordered_map<int,int>mp;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]){
                 st.pop();
             }
-            ans[i] = st.empty() ? -1 : st.top();
-            st.push(vec[i]);
+            if(st.empty()){
+                mp[nums2[i]]=-1;
+            }
+            else{
+                mp[nums2[i]]=st.top();
+            }
+            st.push(nums2[i]);
         }
-        
-        return ans;
-    }
-
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> index_map;
-        for (int i = 0; i < nums2.size(); ++i) {
-            index_map[nums2[i]] = i;
+        vector<int>ans;
+        ans.reserve(nums1.size());
+        for(auto i:nums1){
+            ans.push_back(mp[i]);
         }
-
-        vector<int> vec(nums2.size());
-        for (int i = 0; i < nums1.size(); ++i) {
-            vec[index_map[nums1[i]]] = nums1[i];
-        }
-
-        vector<int> next_greater = findGreatest(nums2);
-        vector<int> ans(nums1.size());
-        for (int i = 0; i < nums1.size(); ++i) {
-            ans[i] = next_greater[index_map[nums1[i]]];
-        }
-
         return ans;
     }
 };
